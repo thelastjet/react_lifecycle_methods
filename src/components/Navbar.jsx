61e3dfd230d7736ec4cd249da415dac51497df6f
Ben <bbryant@truecoders.io>
2021-02-11 08:44:51 -0600

    Removed unnecessary components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,20 @@
-import * as React from "react";
+import React, { Component } from "react";
 
-const Navbar = (props) => {
-  console.log("Navbar - Rendered");
-  return (
-    <nav style={{ backgroundColor: "#0094C8", color: "#eee", padding: "1rem" }}>
-      <span>React Lifecycle Methods</span>
-    </nav>
-  );
-};
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log("Navbar - Rendered");
+    return (
+      <nav
+        style={{ backgroundColor: "#0094C8", color: "#eee", padding: "1rem" }}
+      >
+        <span>React Lifecycle Methods</span>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
