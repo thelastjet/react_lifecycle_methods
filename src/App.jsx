096d2e8662726c59a60810b61ec05f17d5bc08ea
Jay Thomas <thelastjet17@gmail.com>
2021-07-08 22:55:26 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App () {
 
-    //initialize state
-    this.state = { text: "Let's practice React!", hasLoaded: false }; 
-    //bind methods to this
-    this.handleInput = this.handleInput.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+  const [text, setText] = React.useState("Welcome to React!");
+  const [hasLoaded, setHasLoaded] = React.useState(false);
+
+  function handleInput (e) {
+    console.log("Changing State - text");
+    setText(e.target.value)
   }
 
-  handleInput (e) {
-    this.setState({ text: e.target.value })
+  function handleClick () {
+    console.log("Changing State - hasLoaded");
+    setHasLoaded(hasLoaded ? false : true);
   }
+  
+  React.useEffect(() => {
+    console.log("App - Effect Hook");
+    setHasLoaded(true);
+    return function cleanup() {
+      console.log("App - Cleanup");
+    };
+  }, []);
 
-  handleClick () {
-    this.setState({ hasLoaded: !this.state.hasLoaded })
-  }
-  //sets hasLoaded to true so I get my first return in render()
-  componentDidMount () {
-    this.setState({ hasLoaded: true })
-  }
-
-  render() {
-    if (this.state.hasLoaded) {
-      return (
-        <div>
-          <h1>{this.state.text}</h1>
-          <hr />
-          <input 
-            type="text" 
-            placeholder="Enter some text here!" 
-            onChange={this.handleInput} />    
-          <br />
-          <br />
-          <button onClick={this.handleClick}>Unload</button>
-        </div>
-      ) 
-    } else {
-      return (
-            <div>
-              <h1>...loading</h1>
-              <br />
-              <button onClick={this.handleClick}>Load</button>
-            </div>
-      )
-    }
+  if (hasLoaded) {
+    return (
+      <div>
+        <h1>{text}</h1>
+        <hr />
+        <input 
+          type="text" 
+          placeholder="Enter some text here!" 
+          onChange={handleInput} />    
+        <br />
+        <br />
+        <button onClick={handleClick}>Unload</button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>...loading</h1>
+        <br />
+        <button onClick={handleClick}>Load</button>
+      </div>
+    )
   }
 }
 
